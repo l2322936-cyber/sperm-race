dbbadadbdce44e2ef2fd369e2a68505b59371d68
l2322936-cyber <l2322936@gmail.com>
2026-01-22 16:47:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
 
+document.getElementById("startBtn").onclick = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "lime";
+  ctx.font = "30px Arial";
+  ctx.fillText("NEW CODE LOADED ✅", 220, 250);
+};
